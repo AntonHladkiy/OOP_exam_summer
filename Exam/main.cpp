17f32fe0aa985f68d1d33c313b310facd5a5ac37
Anton Hladkyi <gladkyy@ukr.net>
2020-06-08 13:59:48 +0300

    deletion added

--- Exam/main.cpp
+++ Exam/main.cpp
@@ -1,10 +1,11 @@
 #include "BTree/Btree.h"
-
+#include "AVLTree/AVLTree.h"
 int main() {
-	BTree<int> tree(3);
+	BTree<int> tree(4);
 	for (int i = 0; i < 100; i++) {
 		tree.insert(i);
 	}
-	tree.insert(0);
+	tree.traverse();
+	tree.remove(4);
 	tree.traverse();
 }
